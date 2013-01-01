8ede1881d4ad39e4ba9de8be851c39348294e685
jared <jared@macbook.ri.cox.net>
2012-12-31 22:18:58 -0500

    Working parser that will get message prefixes.

--- client/src/com/test9/irc/parser/Parser.java
+++ client/src/com/test9/irc/parser/Parser.java
@@ -4,43 +4,27 @@ import java.util.Arrays;
 
 public class Parser {
 
-	final private String PREFIXES[] = {"ERR", "RPL"};
 	private static boolean init = false;
-
-	final static private String ERR[] = {"401_ERR_NOSUCHNICK", "402_ERR_NOSUCHSERVER", "403_ERR_NOSUCHCHANNEL", 
-		"404_ERR_CANNOTSENDTOCHAN", "405_ERR_TOOMANYCHANNELS", "406_ERR_WASNOSUCHNICK", "407_ERR_TOOMANYTARGETS", 
-		"409_ERR_NOORIGIN", "411_ERR_NORECIPIENT", "412_ERR_NOTEXTTOSEND", "413_ERR_NOTOPLEVEL", "414_ERR_WILDTOPLEVEL",
-		"421_ERR_UNKNOWNCOMMAND", "422_ERR_NOMOTD", "423_ERR_NOADMININFO", "424_ERR_FILEERROR", "431_ERR_NONICKNAMEGIVEN", 
-		"432_ERR_ERRONEUSNICKNAME", "433_ERR_NICKNAMEINUSE", "436_ERR_NICKCOLLISION", "441_ERR_USERNOTINCHANNEL", 
-		"442_ERR_NOTONCHANNEL", "443_ERR_USERONCHANNEL", "444_ERR_NOLOGIN", "445_ERR_SUMMONDISABLED", "446_ERR_USERSDISABLED", 
-		"451_ERR_NOTREGISTERED", "461_ERR_NEEDMOREPARAMS", "462_ERR_ALREADYREGISTRED", "463_ERR_NOPERMFORHOST", "464_ERR_PASSWDMISMATCH", 
-		"465_ERR_YOUREBANNEDCREEP", "467_ERR_KEYSET", "471_ERR_CHANNELISFULL", "472_ERR_UNKNOWNMODE", "473_ERR_INVITEONLYCHAN", 
-		"474_ERR_BANNEDFROMCHAN", "475_ERR_BADCHANNELKEY", "481_ERR_NOPRIVILEGES", "482_ERR_CHANOPRIVSNEEDED",
-		"483_ERR_CANTKILLSERVER", "491_ERR_NOOPERHOST", "501_ERR_UMODEUNKNOWNFLAG", "502_ERR_USERSDONTMATCH"};
-
-	final static private String RPL[] = {"300_RPL_NONE", "302_RPL_USERHOST", "303_RPL_ISON", "301_RPL_AWAY", 
-		"305_RPL_UNAWAY", "306_RPL_NOWAWAY", "311_RPL_WHOISUSER", "312_RPL_WHOISSERVER",
-		"313_RPL_WHOISOPERATOR", "317_RPL_WHOISIDLE", "318_RPL_ENDOFWHOIS", "319_RPL_WHOISCHANNELS", "314_RPL_WHOWASUSER", 
-		"369_RPL_ENDOFWHOWAS", "321_RPL_LISTSTART", "322_RPL_LIST", "323_RPL_LISTEND", "324_RPL_CHANNELMODEIS", 
-		"331_RPL_NOTOPIC", "332_RPL_TOPIC", "341_RPL_INVITING", "342_RPL_SUMMONING", "351_RPL_VERSION, WHOREPLY",
-		"315_RPL_ENDOFWHO", "353_RPL_NAMREPLY", "366_RPL_ENDOFNAMES", "364_RPL_LINKS", "365_RPL_ENDOFLINKS", "367_RPL_BANLIST", 
-		"368_RPL_ENDOFBANLIST", "371_RPL_INFO", "374_RPL_ENDOFINFO", "375_RPL_MOTDSTART", "372_RPL_MOTD", "376_RPL_ENDOFMOTD", 
-		"381_RPL_YOUREOPER", "382_RPL_REHASHING", "391_RPL_TIME", "392_RPL_USERSSTART", "393_RPL_USERS", "394_RPL_ENDOFUSERS",
-		"395_RPL_NOUSERS", "200_RPL_TRACELINK", "201_RPL_TRACECONNECTING", "202_RPL_TRACEHANDSHAKE", "203_RPL_TRACEUNKNOWN", 
-		"204_RPL_TRACEOPERATOR", "205_RPL_TRACEUSER", "206_RPL_TRACESERVER", "208_RPL_TRACENEWTYPE", "261_RPL_TRACELOG", 
-		"211_RPL_STATSLINKINFO", "212_RPL_STATSCOMMANDS", "213_RPL_STATSCLINE", "214_RPL_STATSNLINE", "215_RPL_STATSILINE", 
-		"216_RPL_STATSKLINE", "218_RPL_STATSYLINE", "219_RPL_ENDOFSTATS", "241_RPL_STATSLLINE", "242_RPL_STATSUPTIME", 
-		"243_RPL_STATSOLINE", "244_RPL_STATSHLINE", "221_RPL_UMODEIS", "251_RPL_LUSERCLIENT", "252_RPL_LUSEROP", "253_RPL_LUSERUNKNOWN", 
-		"254_RPL_LUSERCHANNELS", "255_RPL_LUSERME", "256_RPL_ADMINME", "257_RPL_ADMINLOC1", "258_RPL_ADMINLOC2", "259_RPL_ADMINEMAIL"};
+	private static String prefix = "";
+	private static String command = "";
+	private static String params = "";
+	private static String server_name = "";
+	private static String nickname = "";
+	private static String user = "";
+	private static String host = "";
 
 	public static void main(String[] args)
 	{
 		Parser p = new Parser();
-		
-		System.out.println(p.parse(":irc.ecsig.com 256 jared-test :Welcome to the " +
-				"ECSIG IRC Network jared-test!jared-test@ip68-110-207-195.ri.ri.cox.net"));
+
+		System.out.println(p.parse(":irc.ecsig.com 005 jared-test CMDS=KNOCK,MAP,DCCALLOW,USERIP " +
+				"UHNAMES NAMESX SAFELIST HCN MAXCHANNELS=50 CHANLIMIT=#:50 MAXLIST=b:60,e:60,I:60 " +
+				"NICKLEN=30 CHANNELLEN=32 TOPICLEN=307 KICKLEN=307 AWAYLEN=307 :are supported " +
+				"by this server"));
+
+		System.out.println(p.parse(":jared-test!jared-test@somehost 255 jared-test :I have 12 clients and 1 servers"));
 	}
-	
+
 	public Parser() 
 	{
 		init = true;
@@ -48,71 +32,47 @@ public class Parser {
 
 	public String parse(String message)
 	{
-		String prefix_msg_seperation[] = message.split(":");
-		System.out.println(Arrays.toString(prefix_msg_seperation));
-		String prefix_split[] = prefix_msg_seperation[1].split(" ");
-		String server = prefix_split[0];
-		String irc_code = prefix_split[1];
-		String nick = prefix_split[2];
-		String irc_prot_message = "";
-		if(Integer.parseInt(irc_code) >= 401 && Integer.parseInt(irc_code) <= 502)
-			irc_prot_message = find_full_error(irc_code);
-		else if(Integer.parseInt(irc_code) >= 200 && Integer.parseInt(irc_code) <= 395)
-			irc_prot_message = find_full_rpl(irc_code);
+		String message_split[] = message.split(" ", 2);
 
+		if(message.startsWith(":"))
+			get_prefix(message_split[0]);
 
-		return irc_prot_message;
+		System.out.println("nickname:'"+nickname + "' user:'"+user+"' host'"+host+
+				"'server_name'"+server_name+"'");
+		return "";
 	}
 
-
-	private String find_full_error(String irc_code)
+	private void get_prefix(String prefix)
 	{
-		String which_error = "";
+		prefix = prefix.substring(1);
+		String split_prefix[] = prefix.split("[!@ ]");
+		System.out.println(Arrays.toString(split_prefix));
 
-		boolean found = false;
-		int try_index = 0;
-
-		while(!found && try_index < ERR.length)
-		{
-			if(ERR[try_index].startsWith(irc_code, 0))
-			{
-				found = true;
-				which_error = ERR[try_index];
-			}
-			else
-				try_index++;
+		if(split_prefix.length == 3) {
+			nickname = split_prefix[0];
+			user = split_prefix[1];
+			host = split_prefix[2];
 		}
-
-		if(found)
-			return which_error;
+		else if(split_prefix.length == 2)
+		{
+			nickname = split_prefix[0];
+			host = split_prefix[1];
+		}
 		else
-			return new String("[ERROR]: com.test9.irc.parser/Parser.java Could not find error code.");
+			server_name = split_prefix[0];	
 	}
-
-	private String find_full_rpl(String irc_code)
+	public void reset_parser()
 	{
-		String which_rpl = "";
+		prefix = "";
+		command = "";
+		params = "";
+		server_name = "";
+		nickname = "";
+		user = "";
+		host = "";
 
-		boolean found = false;
-		int try_index = 0;
-
-		while(!found && try_index < RPL.length)
-		{
-			if(RPL[try_index].startsWith(irc_code, 0))
-			{
-				found = true;
-				which_rpl = RPL[try_index];
-			}
-			else
-				try_index++;
-		}
-
-		if(found)
-			return which_rpl;
-		else
-			return new String("[ERROR]: com.test9.irc.parser/Parser.java Could not find reply code.");
 	}
-	
+
 	/**
 	 * @return the init
 	 */
